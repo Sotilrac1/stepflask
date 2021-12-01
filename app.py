52a88cf5c76d6bd7f3460365c1615dacdfcb3f44
carlos <cf8838@gmail.com>
2021-12-01 16:40:01 -0500

    commit

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
-from flask import Flask, render_template, redirect, session, url_for,request
+from flask import Flask, render_template, redirect, session, url_for,request, flash
+from datetime import timedelta
+import sqlalchemy 
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
         session["user"] = user
         return redirect(url_for("user"))
     else:
-        return render_template("index.html")
+        if "user" in session:
+            return redirect(url_for("user"))
+        
+        return render_template("login.html")
     
 @app.route("/user")
 def user():
     if "user" in session:
         user = session["user"]     
         return f"<h1>{user}</h1>"
-    else:
-        return redirect("index.html")
+    else:    
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    flash("you have been logged out!", "info")
+    return redirect(url_for('login'))
+    
+    
+    
+    
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
